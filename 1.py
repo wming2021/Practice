fbecf87862badb7d88618c0451dc34fdb52d2d9f
wming2021 <wmddream2013@qq.com>
2021-03-17 01:28:14 +0800

    无他，唯手熟尔

---
+++ 1.py
@@ -0,0 +1,20 @@
+
+
+def decorator_1(func):
+    def good(a, b):
+        ret = func(a, b)
+        return abs(ret)
+    return good
+
+
+@decorator_1
+def add(add_a, add_b):
+    if add_a > add_b:
+        return add_a + add_b
+    else:
+        return add_a - add_b
+
+
+print(add(1, 6))
+
+
